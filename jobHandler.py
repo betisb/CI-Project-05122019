f5a5a7b262a356a1ca706156675d2b8f6631062b
Ken <kmessne3@kent.edu>
2019-10-28 21:45:24 -0400

    Start to the job handler

---
+++ jobHandler.py
@@ -0,0 +1,17 @@
+# Having all sorts of issues. I might have to look into a w3 lesson or something after my trip.
+
+print("HPC Project Part One.\n")
+
+continueToRun = 1
+
+While (continueToRun)
+print("What action would you like to perform:/n/n")
+print(" 1 Status: Shows the status of a selected job.\n")
+print(" 2 List: List all currently running jobs.\n")
+print(" 3 Start: Start a new job.\n")
+print(" 4 Stop: Stop a currently running job.\n")
+print(" 5 Quit: Terminate all running process and quit./n/n")
+
+choice = raw_input("Action: ")
+
+print"Your chosen action was " + choice
