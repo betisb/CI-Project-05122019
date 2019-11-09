3e2df4207cf5e4432214e54acc0199de49c5ead8
betisb <bbaheri@kent.edu>
2019-11-09 01:00:38 -0500

    V1.1

---
+++ GC_Initial_Enviroment_Setup_Ubuntu.sh
@@ -0,0 +1,23 @@
+sudo apt install python-pip
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+#Docker Method
+#RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+
+
+gcloud init
+gcloud auth application-default login
+pip install --upgrade pytest, flaky, numpy, oauth2client, google-api-python-clinet
+pip install google-cloud-storage==1.22.0
+pip install google-cloud-pubsub==1.0.0
+pip install gcloud
+gcloud auth login
+export GOOGLE_APPLICATION_CREDENTIALS="/home/ciuser/Documents/bucketkey.json"
+export GCLOUD_PROJECT=cloud-infrastructure-project
